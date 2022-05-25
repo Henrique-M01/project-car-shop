bf7e01e5d19c7ed87b5c1c43a15212db4fbcf4fb
henrique <hickm018@gmail.com>
2022-05-25 11:03:29 -0300

    refactor: change vahicle interface

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,25 +1,11 @@
-export interface Vehicle {
-  model: string;
-  year: number;
-  color: string;
-  status?: boolean;
-  buyValue: number;
-}
+import { z } from 'zod';
 
-// import { z } from 'zod';
+const VehicleSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
 
-// const VehicleSchema = z.object({
-//   model: z.string().min(3, {
-//     message: 'Models name must be 3 or more characters long' }),
-//   year: z.date(),
-//   color: z.string().min(3, {
-//     message: 'Color must be 3 or more characters long' }),
-//   status: z.boolean().optional(),
-//   buyValue: z.number().int(),
-// });
-
-// type Vehicle = z.infer<typeof VehicleSchema>;
-
-// export default Vehicle;
-// export { VehicleSchema }; 
-// Porque isso nao passa no teste????
+export type Vehicle = z.infer<typeof VehicleSchema>;
