87c1095286bcdb5d3be43182a5176da6035dfb6f
henrique <hickm018@gmail.com>
2022-05-25 12:03:40 -0300

    feat: add car service create method

---
+++ src/services/CarsService.ts
@@ -0,0 +1,20 @@
+import { Car, CarSchema } from '../interfaces/CarInterface';
+import Service from '.';
+import CarModel from '../models/CarsModel';
+
+class CarService extends Service<Car> {
+  constructor(model = new CarModel()) {
+    super(model);
+  }
+
+  create = async (obj: Car): Promise<Car | null> => {
+    const parsed = CarSchema.safeParse(obj);
+    if (!parsed.success) {
+      return null;
+    }
+
+    return this.model.create(obj);
+  };
+}
+
+export default CarService;
